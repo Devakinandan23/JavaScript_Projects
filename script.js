c4dc5939e542a61e6965ac9f994f4006a9b61f48
Devakinandan23 <92246269+Devakinandan23@users.noreply.github.com>
2023-09-13 10:55:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+console.log(btnOpenModal);
+
+const openModal = function(){
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+const closeModal = function(){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < btnOpenModal.length; i++)
+{
+  btnOpenModal[i].addEventListener('click',openModal);
+}
+btnCloseModal.addEventListener('click',closeModal);
+overlay.addEventListener('click',closeModal);
+
+
+document.addEventListener('keydown',function(e){
+  console.log(e.key);
+  if(e.key === 'Escape' && !modal.classList.contains('hidden'))
+  {
+    closeModal();
+  }
+});
+
+
+
+
+
+
+
