3ccb0160ec1c54e975008bb20c2263673fe9a026
Devakinandan23 <92246269+Devakinandan23@users.noreply.github.com>
2023-09-13 11:02:24 +0530

    Deleted HTML CSS

--- HTML CSS/pointer hover circular/script.js
+++
@@ -1,9 +0,0 @@
-let title = document.querySelector('.title');
-let curs = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', (e) => {
-  let x = e.pageX;
-  let y = e.pageY;
-  curs.style.left = (x - 22) + "px";
-  curs.style.top = (y - 22) + "px";
-})
