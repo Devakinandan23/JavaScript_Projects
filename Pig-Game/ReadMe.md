59b8c8bbed3bef1f99ff068369b6857ceb74fda3
Devakinandan23 <92246269+Devakinandan23@users.noreply.github.com>
2023-09-14 01:41:13 +0530

    Pig-Game

---
+++ Pig-Game/ReadMe.md
@@ -0,0 +1,46 @@
+# Pig Game
+
+Pig Game is a simple two-player dice game implemented using HTML, CSS, and JavaScript. Players take turns rolling a dice and accumulate points based on the dice rolls. The goal is to reach a certain score (in this case, 25) to win the game.
+
+## How to Play
+
+1. Open the `index.html` file in your web browser.
+2. The game starts with Player 1 (Player 1 is the active player initially).
+3. Click the "Roll dice" button to roll the dice. The number rolled on the dice will be displayed.
+4. If you roll a 1, your current score will be reset to 0, and it will be the next player's turn.
+5. If you roll any other number, that number will be added to your current score.
+6. To hold your current score and add it to your total score, click the "Hold" button.
+7. The first player to reach a total score of 25 or more wins the game.
+8. You can start a new game at any time by clicking the "New game" button.
+
+## Code Structure
+
+The code is organized into HTML, CSS, and JavaScript files:
+
+- `index.html`: This is the main HTML file that defines the structure of the game. It contains the player sections, buttons, and dice image.
+
+- `style.css`: The CSS file contains the styling for the game, including layout, colors, fonts, and button animations.
+
+- `script.js`: The JavaScript file contains the game logic. It handles rolling the dice, updating scores, switching players, and determining the winner.
+
+## How the Game Works
+
+1. The game starts by initializing the scores, current scores, active player, and game state.
+
+2. Clicking the "Roll dice" button generates a random number between 1 and 6 (simulating a dice roll). The dice image updates to display the rolled number.
+
+3. If the rolled number is not 1, it's added to the current player's current score. If it is 1, the current score resets to 0, and the turn switches to the other player.
+
+4. Clicking the "Hold" button adds the current score to the player's total score. If a player's total score reaches 25 or more, they win the game.
+
+5. The game tracks the active player, updates scores, and provides a visual indication of the active player.
+
+6. You can start a new game at any time by clicking the "New game" button, which resets all scores and settings.
+
+## Contrubuters
+
+[Devakinandan23](https://github.com/Devakinandan23)
+
+## Enjoy the Game!
+
+Have fun playing Pig Game! Test your luck and strategy against a friend, and aim to be the first to reach 25 points.
---
+++ Pig-Game/script.js
@@ -0,0 +1,94 @@
+'use strict';
+
+//Score Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let Score,currentScore,ActivePlayer,playing;
+
+const init = function(){
+  Score = [0,0];
+  currentScore = 0;
+  ActivePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+
+init();
+
+const clickOnDice = diceEl.addEventListener('click',function(){
+  diceEl.classList.add('hidden');
+});
+
+
+  const Switching = function(){
+      currentScore = 0;
+      document.getElementById(`current--${ActivePlayer}`).textContent = currentScore;
+      ActivePlayer = ActivePlayer === 0 ? 1 : 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+  }
+  
+  btnRoll.addEventListener('click',function(){
+    if(playing)
+    {
+      let diceRoll = Math.trunc(Math.random()*6 + 1);
+    
+    diceEl.src = `dice-${diceRoll}.png`;
+    diceEl.classList.remove('hidden');
+    if(diceRoll !== 1)
+    {
+      currentScore += diceRoll;
+      
+      document.getElementById(`current--${ActivePlayer}`).textContent = currentScore;
+      // current0El.textContent = currentScore; //Change Later
+    }else{
+      Switching();
+    }
+    }
+  });
+
+
+btnHold.addEventListener('click',function(){
+  if(playing)
+  {
+    //1.Player Score should get updated
+    // score0El.textContent = currentScore;
+    Score[ActivePlayer] += currentScore;
+    document.querySelector(`#score--${ActivePlayer}`).textContent = Score[ActivePlayer];
+
+    if(Score[ActivePlayer] >= 25)
+    {
+      playing = false;
+      document.querySelector(`.player--${ActivePlayer}`).classList.add('player--winner');
+      document.querySelector(`.player--${ActivePlayer}`).classList.remove('player--active');
+      diceEl.classList.add('hidden');
+      FIXME//there is no "." dot in classList, just directly write class name (++)
+    } else{
+      //2.Switching Active Players
+        Switching();
+    } 
+  }
+});
+
+btnNew.addEventListener('click',init);
